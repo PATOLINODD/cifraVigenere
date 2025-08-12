03836be2963ab2c673ab2bf118ccb30f89dca879
PATOLINODD <psodesenhos@gmail.com>
2025-08-12 18:25:11 -0300

    new feature -> alphabet animations added to the background

---
+++ scripts/alphabetAnimation.js
@@ -0,0 +1,20 @@
+const alphabetStack = document.getElementById("alphabetStack");
+
+for(let i = 0; i < window.screen.height / 16; i++){
+    const alphabetRow = document.createElement("div");
+    alphabetRow.classList.add("alphabet-row");
+    const alphabetSpan = document.createElement("span");
+    if(i % 2 == 0) {
+        alphabetSpan.classList.add("animate-to-right");
+    } else {
+        alphabetSpan.classList.add("animate-to-left");
+    }
+    const alphabet = document.createTextNode(`A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+          A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+          A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+          A B C D E F G H I J K L M N O P Q R S T U V W X Y Z`);
+    alphabetSpan.appendChild(alphabet);
+    alphabetSpan.style = "color: hsla(113, 100.00%, 31.00%, 0.05);";
+    alphabetRow.appendChild(alphabetSpan);
+    alphabetStack.appendChild(alphabetRow);
+}
